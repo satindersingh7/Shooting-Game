7312c309f5fb65783dca3faf5f5eaba620cae409
indersat8 <indersat8@outlook.com>
2018-08-20 13:10:48 +1200

    new

--- Shooting Game/Form1.cs
+++ Shooting Game/Form1.cs
@@ -14,44 +14,63 @@ namespace Shooting_Game
     {
         public Form1()
         {
+            // The below line initialized the form appearance.
             InitializeComponent();
         }
 
-       
+       // We are intereted in random number generator.
 
         Random r = new Random();
+
+   
+
+       // int nndNumber = r.Next(0, 6);
+            // We inialised the score with a zero value.
         int Score = 0;
+
+        // We inialised the total_shots with a zero value.
         int total_shots = 0;
+
+        //We inialised the miss_shots with a zero value.
         int miss_shots = 0;
 
-
+        //We inialised the _shot voice .
         void shot_voice()
         {
+            //Sound
             System.Media.SoundPlayer player = new System.Media.SoundPlayer (@"C:\Users\user\Downloads\shot.wav");
             player.Play();
         }
+
+        // Total Score
         void fn_shot ()
         {
+            // We incremented the Score and added Score to the label
             Score++;
             label1.Text = "score=" + Score;
-            total_shots++;
+            
+            total_shots++;  // We incremented the shots and added Shots to the label
             label3.Text = "Total Shots=" + total_shots;
-
+            
             shot_voice();
+            
         }
 
+        // Miss fire
         void fn_miss_shot()
         {
+            //  We incremented the Missing Shots and added Missing Shots to the labe2
             miss_shots++;
             label2.Text = "Missing Shots=" + miss_shots;
-            total_shots++;
+            total_shots++; //  We incremented the Shots and added Shots to the labe2
             label3.Text = "Total Shots=" + total_shots;
-
+           
             shot_voice();
         }
-
+        //Count of Total Shots
         void reset()
         {
+            // code to create a reset funtion
             Score = 0;
             miss_shots = 0;
             total_shots = 0;
@@ -98,6 +117,7 @@ namespace Shooting_Game
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
             fn_shot();
+
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
---
+++ Shootinggametest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Media;
+
+namespace Shootinggametest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\user\Downloads\shot.wav");
+            player.Play();
+            Assert.IsTrue(player != null);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int Score = 0;
+            Assert.IsTrue(Score == 0);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int total_shots = 0;
+            Assert.IsTrue(total_shots == 0);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int miss_shots = 0;
+            Assert.IsTrue(miss_shots == 0);
+        }
+    }
+}
